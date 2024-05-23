923953dc8c5eead6649750c0928b62ae1970115e
Josh Fuery <119151453+jf700@users.noreply.github.com>
2024-05-23 16:12:25 -0700

    Update DinnerMenu.java

--- MP4/src/DinnerMenu.java
+++ MP4/src/DinnerMenu.java
@@ -1,2 +1,26 @@
-public class DinnerMenu {
+import java.util.LinkedList;
+
+public class DinnerMenu extends Menu {
+    public DinnerMenu(LinkedList<String> menuItems) {
+        super(menuItems);
+    }
+
+    public void displayMenuItems() {
+        System.out.println("Dinner Menu:");
+        for (String item : menuItems) {
+            System.out.println("- " + item);
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList<String> items = new LinkedList<>();
+        items.add("Burger");
+        items.add("Pasta");
+        items.add("Pizza");
+
+        DinnerMenu dinnerMenu = new DinnerMenu(items);
+
+        dinnerMenu.displayMenuItems();
+    }
+
 }
