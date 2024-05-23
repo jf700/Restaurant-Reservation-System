2328aabc1029cb22cb9071d905116dc3df58ebd3
calefalesse <143558352+calefalesse@users.noreply.github.com>
2024-05-22 20:10:10 -0700

    Update LunchMenu.java

--- MP4/src/LunchMenu.java
+++ MP4/src/LunchMenu.java
@@ -1,2 +1,27 @@
-public class LunchMenu {
+import java.util.LinkedList;
+
+public class LunchMenu extends Menu {
+    public LunchMenu(LinkedList<String> menuItems) {
+        super(menuItems);
+    }
+
+    public void displayMenuItems() {
+        System.out.println("Lunch Menu:");
+        for (String item : menuItems) {
+            System.out.println("- " + item);
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList<String> items = new LinkedList<>();
+        items.add("Grilled Cheese Sandwich");
+        items.add("Caesar Salad");
+        items.add("Tomato Soup");
+        items.add("Fries");
+
+        LunchMenu lunchMenu = new LunchMenu(items);
+        
+        lunchMenu.displayMenuItems();
+    }
+
 }
