052bf5f4d27053ad4443af915f248fc300bbb4fb
calefalesse <143558352+calefalesse@users.noreply.github.com>
2024-05-22 20:03:59 -0700

    Update Menu.java

--- MP4/src/Menu.java
+++ MP4/src/Menu.java
@@ -1,2 +1,21 @@
-public class Menu {
+import java.util.LinkedList;
+
+public abstract class Menu {
+    protected LinkedList<String> menuItems;
+
+    public Menu(LinkedList<String> menuItems) {
+        this.menuItems = menuItems;
+    }
+
+    public abstract void displayMenuItems();
+
+    public LinkedList<String> getMenuItems() {
+        return menuItems;
+    }
+
+    public void setMenuItems(LinkedList<String> menuItems) {
+        this.menuItems = menuItems;
+    }
+
 }
+
